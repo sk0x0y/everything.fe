7903f5fede09321cc0e2e55e6285f8e56e1ba887
Ray <rayxkr@myutex.com>
2020-06-05 01:50:06 +0900

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,110 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <header>
+      <h1 class="brand">에브리띵</h1>
+      <ul class="menu">
+        <li><router-link to="/">공지사항</router-link></li>
+        <li><router-link to="/">자유게시판</router-link></li>
+        <li><router-link to="/">고객센터</router-link></li>
+      </ul>
+      <div class="search-form">
+        <input type="text" placeholder="검색어를 입력해주세요" />
+        <button type="button">검색</button>
+      </div>
+      <ul class="member">
+        <li><router-link to="/">기존회원</router-link></li>
+        <li><router-link to="/">신규회원</router-link></li>
+      </ul>
+    </header>
+    <main>
+      <section class="left-section">
+        <div class="wrap">
+          <nav>현재 위치: 블로그 / 신규 소식</nav>
+        </div>
+      </section>
+      <section class="right-section">
+        <div class="wrap">safsd</div>
+      </section>
+    </main>
+    <footer>
+      <h1 class="brand">에브리띵</h1>
+      <p>Copyright &copy; 에브리띵, All Rights Reserved.</p>
+      <p>커뮤니티에 올라온 게시물의 모든 책임은 게시물 작성자에게 있습니다.</p>
+    </footer>
   </div>
 </template>
 
 <style lang="scss">
+@import '~open-color/open-color.scss';
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  font-family: 'Gothic A1', 'sans-serif';
+  font-weight: 400;
+  font-size: 14px;
 }
 
-#nav {
-  padding: 30px;
+header {
+  display: flex;
+  height: 70px;
+  line-height: 70px;
+  text-align: center;
+  border-bottom: 1px solid $oc-gray-2;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+  h1.brand {
+    margin-left: 60px;
+  }
 
-    &.router-link-exact-active {
-      color: #42b983;
+  ul.menu {
+    margin: 0 0 0 auto;
+
+    li {
+      margin-left: 20px;
     }
   }
+
+  div.search-form {
+    margin-left: 30px;
+
+    input {
+      width: 200px;
+    }
+  }
+
+  ul.member {
+    margin: 0 30px;
+
+    li {
+      margin-left: 10px;
+    }
+  }
+}
+
+main {
+  display: flex !important;
+
+  section {
+    &.left-section {
+      flex-basis: 60%;
+      height: 1000px;
+      border-right: 1px solid $oc-gray-1;
+
+      div.wrap {
+        width: 700px;
+        margin-left: auto;
+        margin-right: 30px;
+      }
+    }
+
+    &.right-section {
+      flex-basis: 40%;
+    }
+
+    nav {
+      border-bottom: 1px solid $oc-gray-1;
+    }
+  }
+}
+
+footer {
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import 'normalize.css/normalize.css';
+import './common.scss';
 
 Vue.config.productionTip = false;
 
